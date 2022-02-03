7811a9f689e6232ff3cf68bc92b0ed4351803ba4
David Uebelacker <david.endress@endress.com>
2022-02-03 13:36:24 +0100

    :ok_hand: added spring, faker and factory bot

--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -3,7 +3,9 @@
 require 'test_helper'
 
 class TodoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "todo test factory should work" do
+    todo = create(:todo)
+    puts todo.text
+    puts todo.status
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,15 +3,14 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require 'faker'
+
+FactoryBot.find_definitions if FactoryBot.factories.count == 0
 
 module ActiveSupport
   class TestCase
-    # Run tests in parallel with specified workers
+    include FactoryBot::Syntax::Methods
     parallelize(workers: :number_of_processors)
-
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
-
-    # Add more helper methods to be used by all tests here...
   end
 end
